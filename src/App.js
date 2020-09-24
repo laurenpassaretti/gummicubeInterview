e363f3e9f1eef0e0b0ebe4d87438aa52b8f70a45
Lauren Doss <doss.laurene@gmail.com>
2020-09-24 15:04:14 -0700

    changed function structure and moved button

--- src/App.js
+++ src/App.js
@@ -4,44 +4,35 @@ import { Alert, Jumbotron, Container, Button } from 'reactstrap'
 
 
 function App() {
-  
-const[visible, setVisible] = useState(true); 
+
+    const [visible, setVisible] = useState(false);
+    const onDismiss = () => setVisible(false);
+    const handleButtonClick = () => setVisible(true);
+
+    return (
 
 
-const DisplayAlert = (props) =>{
-  console.log("was clicked!")
-  const onDismiss =()=> setVisible(false)
-  return (
-    
-    <Alert color="warning" isOpen={visible} toggle={onDismiss}>
-      I am an alert and I can be dismissed!
-    </Alert>
-
-      
-  )}; 
-
-  return (
-
-    
-    <div className="App">
-      <Alert color="primary" isOpen="true">
-        This is a primary alert
+        <div className="App">
+            <Alert color="primary" isOpen="true">
+                This is a primary alert
       </Alert>
 
-      <Jumbotron fluid>
-        <Container fluid>
-        <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-          </Container>
-      </Jumbotron>
-      <Button type="submit" outline color="primary" onClick={DisplayAlert}>
-      Click me for an alert
+            <Jumbotron fluid>
+                <Container fluid>
+                    <h1 className="display-3">Fluid jumbotron</h1>
+                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </Container>
+            </Jumbotron>
+            <Button type="submit" outline color="primary" onClick={handleButtonClick}>
+                Click me for an alert
       </Button>
+            <Alert color="warning" isOpen={visible} toggle={onDismiss}>
+                I am an alert and I can be dismissed!
+    </Alert>
 
-    
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
